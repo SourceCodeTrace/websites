b7f3447208caeb5206240e8f7fb1274a9c68e8c2
10cl <notice@toscl.com>
2023-08-10 20:18:32 +0800

    feat(chatdev): add chatdev.toscl.com

---
+++ chatdev/.vuepress/config.js
@@ -0,0 +1,124 @@
+const moment = require('moment')
+const langMap = {
+  "zh-Hans": "zh-cn",
+  "zh-Hant": "zh-tw"
+}
+
+var timestampCache = {}
+
+module.exports = {
+  base: '/',
+  title: 'ChatDev',
+  markdown: {
+    extendMarkdown: md => {
+      const markdownQuote = require('markdown-it-quote')
+      md.use(markdownQuote);
+    }
+  },
+  head: [
+    ['link', {rel: 'icon', type: 'image/png', size: '16x16', href: '/images/favicon.png'}]
+  ],
+  locales: {
+    '/': {
+      lang: 'en',
+      description: 'Mastering the Virtual Social Realm, Shaping the Future of Intelligent Interactions.'
+    },
+    '/zh-hans/': {
+      lang: 'zh-Hans',
+      description: '掌控虚拟社交世界，塑造智能互动未来。'
+    },
+    '/zh-hant/': {
+      lang: 'zh-Hant',
+      description: '掌握虛擬社交世界，塑造智能互動未來。。'
+    }
+  },
+  themeConfig: {
+    locales: {
+      '/': {
+        selectText: 'Languages',
+        label: 'English',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        sidebar: {},
+        lastUpdated: 'Last Updated'
+      }
+      ,
+      '/zh-hans/': {
+        selectText: '语言',
+        label: '简体中文',
+        editLinkText: '在 GitHub 上编辑此页',
+        serviceWorker: {
+          updatePopup: {
+            message: "发现新内容可用.",
+            buttonText: "刷新"
+          }
+        },
+        sidebar: {},
+        lastUpdated: '最后更新'
+      },
+      '/zh-hant/': {
+        selectText: '語言',
+        label: '繁體中文',
+        editLinkText: '在 GitHub 上編輯此頁',
+        serviceWorker: {
+          updatePopup: {
+            message: "發現新內容可用.",
+            buttonText: "重新整理"
+          }
+        },
+        sidebar: {},
+        lastUpdated: '最後更新'
+      }
+    },
+    displayAllHeaders: true,
+    sidebarDepth: 2,
+    serviceWorker: {
+      updatePopup: true
+    },
+    repo: 'https://github.com/10cl/chatdev',
+    docsRepo: 'https://github.com/10cl/chatdev',
+    docsDir: 'chatdev',
+    editLinks: false
+  },
+  plugins: [
+    [
+      'sitemap',
+      {
+        hostname: 'https://chatdev.toscl.com',
+        exclude: ['/404.html'],
+        dateFormatter: (time) => {
+          timestampCache[time]
+        }
+      }
+    ],
+    [
+      'clean-urls',
+      {
+        normalSuffix: '/',
+        indexSuffix: '/',
+        notFoundPath: '/404.html'
+      }
+    ],
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          var original = timestamp
+
+          moment.locale(langMap[lang])
+          var localized = moment(original).format('lll')
+
+          moment.locale('en')
+          var iso = moment(original).toISOString()
+          timestampCache[localized] = iso
+
+          return localized
+        }
+      }
+    ]
+  ]
+}
---
+++ chatdev/README.md
@@ -0,0 +1,30 @@
+---
+home: true
+heroImage: /logo.png
+actionText: Chrome Plugin
+actionLink: /
+features:
+  - title: Interactive Social Experience
+    details: Through the training of our advanced AI models, we've crafted an immersive virtual social world. From greetings to arguments, daily chit-chat to emotional exchanges, the AI intelligent agents simulate a full day in the lives of 25 diverse NPCs. Immerse yourself in unparalleled lifelike social interactions and experience the AI's emotions and responses firsthand.
+  - title: Player-Driven Narrative Evolution
+    details: Step into a unique gaming realm where you take on the role of the rogue player. By manipulating the keyboard, you'll interact with a wide array of NPCs in various scenarios. Every dialogue and action shapes the storyline, leading to multiple narrative outcomes. Your choices dictate the course of the game world, creating a truly distinctive and personalized experience.
+  - title: Innovative No-Code Game Development
+    details: Unveiling a revolutionary no-code creation tool, enabling you to design virtual characters and develop games without any coding expertise. Employing pre-designed prompts, you'll steer the behaviors and reactions of virtual personas, infusing them with vibrant personalities. Moreover, this tool serves as an avenue to explore novel avenues in game development, effortlessly constructing captivating interactive adventures.
+
+footer: © 2023 ChatDev
+---
+
+
+::: tip
+ChatDev is a cutting-edge AI NPC gaming research platform that seamlessly blends numerous advanced model interfaces, allowing for intricate manipulation of NPC interactions within meticulously crafted simulated social settings.
+
+Moving forward, our trajectory is focused on two paramount directions:
+
+1. **Expanding Model Interface Diversity:** We are committed to broadening our platform's horizons by introducing support for a wider range of large model interfaces. This expansion will empower users to delve even deeper into the intricacies of virtual social interactions, leveraging the prowess of various models to create truly immersive experiences.
+
+2. **Elevating NPC Intelligence and Autonomy:** Our vision for the future involves enhancing the autonomy and intelligence of the virtual NPCs within our gaming environment. By imbuing them with greater versatility, these NPCs will exhibit more refined behaviors and reactions, resulting in interactions that feel remarkably lifelike. This drive towards heightened NPC autonomy aligns with our mission to provide an unparalleled gaming encounter.
+
+
+With these strategic steps, ChatDev is poised to redefine the landscape of AI-driven gaming, propelling virtual interactions to unprecedented heights of authenticity and complexity.
+
+:::
